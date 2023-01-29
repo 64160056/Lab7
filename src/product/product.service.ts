6fdd030e1524fb864d7d449db1cb8dfa8681e3fc
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-29 22:58:42 +0700

    findOne Prodduct

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 
-const product: Product[] = [
+const products: Product[] = [
   { id: 1, name: 'product1', price: 500 },
   { id: 2, name: 'product2', price: 1500 },
   { id: 3, name: 'product3', price: 1000 },
@@ -16,15 +16,21 @@ export class ProductService {
       id: lastProductId++,
       ...createProductDto, //login, name, password
     };
-    product.push(newProduct);
+    products.push(newProduct);
     return newProduct;
   }
 
   findAll() {
-    return product;
+    return products;
   }
 
   findOne(id: number) {
+    const index = products.findIndex((product) => {
+      return product.id === id;
+    });
+    if (index < 0) {
+      throw new NotFoundException();
+    }
     return `This action returns a #${id} product`;
   }
 
