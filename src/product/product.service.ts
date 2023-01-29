3ce3098b03fb87f7c721c8b3b6dab61b2a5962d6
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-29 22:49:05 +0700

    add products

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
+const product: Product[] = [
+  { id: 1, name: 'product1', price: 500 },
+  { id: 2, name: 'product2', price: 1500 },
+  { id: 3, name: 'product3', price: 1000 },
+];
+let lastProductId = 4;
 @Injectable()
 export class ProductService {
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const newProduct: Product = {
+      id: lastProductId++,
+      ...createProductDto, //login, name, password
+    };
+    product.push(newProduct);
+    return newProduct;
   }
 
   findAll() {
-    return `This action returns all product`;
+    return product;
   }
 
   findOne(id: number) {
