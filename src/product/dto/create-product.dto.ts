d4a1d252bf37257cc7d6c361217831d2a7182b19
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-29 23:31:23 +0700

    Validation Product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, MinLength, IsPositive } from 'class-validator';
 export class CreateProductDto {
+  @IsNotEmpty()
+  @MinLength(8)
   name: string;
+
+  @IsNotEmpty()
+  @IsPositive()
   price: number;
 }
