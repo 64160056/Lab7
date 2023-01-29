483fb4b2b9bb07c858192fca46075ae50dd549da
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-29 22:31:23 +0700

    finish Validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, MinLength, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(5)
   login: string;
+
+  @IsNotEmpty()
+  @MinLength(5)
   name: string;
+
+  @Matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)
+  @MinLength(8)
+  @IsNotEmpty()
   password: string;
 }
